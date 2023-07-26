a8c6cadf6ec951b057340bd1025d72ac2dfde139
entest <entest>
2023-07-27 06:26:39 +0700

    test

---
+++ src/get-stock-process.py
@@ -0,0 +1,49 @@
+# haimtran 21/07/2023
+# get record from a stream 
+import boto3
+import os 
+from multiprocessing import Process 
+
+# parameterse 
+REGION = "ap-southeast-1"
+STREAM_NAME = "stock-input-stream"
+NUM_CONSUMER = 10
+
+# kinesis client
+client = boto3.client("kinesis", region_name=REGION)
+
+# get records
+def get_records(max_records=10000):
+    """
+    get records from a stream
+    """
+    response = client.get_shard_iterator(
+        StreamName = STREAM_NAME,
+        ShardId="shardId-000000000001",
+        ShardIteratorType="LATEST"
+    )
+
+    # shard interator 
+    shard_iterator = response["ShardIterator"]
+
+    # get records
+    record_count = 0
+    while record_count < max_records: 
+        response = client.get_records(
+            ShardIterator = shard_iterator,
+            Limit=10
+        )
+        # record
+        records = response["Records"]
+        # next iterator 
+        shard_iterator = response["NextShardIterator"]
+        # 
+        print("id {0} records {1}".format(os.getpid(), records))
+        record_count += len(records)
+        # time.sleep(1)
+
+
+if __name__=="__main__":
+    # get_records()
+    for k in range(1, NUM_CONSUMER):
+            Process(target=get_records).start()
